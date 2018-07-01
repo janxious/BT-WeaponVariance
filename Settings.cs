617f2aacb8959da1937c255cb50b6d7ab418cf28
Joel Meador <joel@expectedbehavior.com>
2018-07-01 13:20:50 -0400

    initial dump of laser effect damage variance code. abandon hope all ye who enter

---
+++ Settings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace WeaponVariance
+{
+    public class Settings
+    {
+        public const string ModName = "WeaponVariance";
+        public const string ModId   = "com.joelmeador.WeaponVariance";
+
+        public bool debug = false;
+
+        public string[] variancePerShot = { };
+        public string[] VariancePerShot => variancePerShot;
+    }
+}
