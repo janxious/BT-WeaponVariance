a5be4ef9da76cc45cab2ca72650dbd97f0054034
Joel Meador <joel@expectedbehavior.com>
2018-07-03 03:32:11 -0400

    convert random number generator to be iterative instead of recursive in the miss state, with a bounded execution count

--- WeaponVariance.cs
+++ WeaponVariance.cs
@@ -62,16 +62,22 @@ namespace WeaponVariance
 
         private static float NormalDistibutionRandom(VarianceBounds bounds, int step = -1)
         {   // compute a random number that fits a gaussian function https://en.wikipedia.org/wiki/Gaussian_function
-            var rand1 = Random.value;
-            var rand2 = Random.value;
-            var gaussian_number = Mathf.Sqrt(-2 * Mathf.Log(rand1)) * Mathf.Cos(2 * Mathf.PI * rand2);
-            var mean = (bounds.max + bounds.min) / 2;
-            var random_number = (gaussian_number * bounds.standardDeviation) + mean;
-            if (step > 0) random_number = Mathf.RoundToInt(random_number / step) * step;
-            if (random_number < bounds.min || random_number > bounds.max) {
-                random_number = NormalDistibutionRandom(bounds, step);
-            }
-            return random_number;
+            // iterative w/ limit adapted from https://natedenlinger.com/php-random-number-generator-with-normal-distribution-bell-curve/
+            const int iterationLimit = 10;
+            var iterations = 0;
+            float randomNumber;
+            do
+            {
+                var rand1 = Random.value;
+                var rand2 = Random.value;
+                var gaussianNumber = Mathf.Sqrt(-2 * Mathf.Log(rand1)) * Mathf.Cos(2 * Mathf.PI * rand2);
+                var mean = (bounds.max + bounds.min) / 2;
+                randomNumber = (gaussianNumber * bounds.standardDeviation) + mean;
+                if (step > 0) randomNumber = Mathf.RoundToInt(randomNumber / step) * step;
+                iterations++;
+            } while ((randomNumber < bounds.min || randomNumber > bounds.max) && iterations < iterationLimit);
+            if (iterations == iterationLimit) randomNumber = (bounds.min + bounds.max) / 2.0f;
+            return randomNumber;
         }
 
         public static float VariantDamage(WeaponEffect weaponEffect, DesignMaskDef designMask)
