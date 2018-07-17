c2e90c23000ea70105a31e14a6eff81f700bef5e
Joel Meador <joel@expectedbehavior.com>
2018-07-16 20:37:21 -0400

    move utility class to its own file, add some new loggers for checking IL out (thought it's not as good as the built-in harmony bits

--- Logger.cs
+++ Logger.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using Harmony;
 
 namespace WeaponVariance
 {
@@ -31,6 +35,31 @@ namespace WeaponVariance
         {
             writer.WriteLine($"Date: {DateTime.Now}");
             writer.WriteLine(new string(c: '-', count: 80));
+        }
+
+        public static void ListTheStack(StringBuilder sb, List<CodeInstruction> codes)
+        {
+            sb.AppendLine(new string(c: '-', count: 80));
+            for (var i = 0; i < codes.Count(); i++)
+            {
+                sb.Append($"{codes[i].opcode}\t\t");
+                if (codes[i].operand != null)
+                {
+                    sb.Append($"{codes[i].operand}");
+                }
+
+                sb.Append(Environment.NewLine);
+            }
+            sb.AppendLine(new string(c: '-', count: 80));
+        }
+
+        public static void LogStringBuilder(StringBuilder sb)
+        {
+            if (sb.Length > 0)
+            {
+                FileLog.Log(sb.ToString());
+                sb.Remove(0, sb.Length);
+            }
         }
     }
 }
---
+++ Utility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace WeaponVariance
+{
+    public static class Utility
+    {
+        internal static float NormalDistibutionRandom(VarianceBounds bounds, int step = -1)
+        {
+            // compute a random number that fits a gaussian function https://en.wikipedia.org/wiki/Gaussian_function
+            // iterative w/ limit adapted from https://natedenlinger.com/php-random-number-generator-with-normal-distribution-bell-curve/
+            const int iterationLimit = 10;
+            var iterations = 0;
+            float randomNumber;
+            do
+            {
+                var rand1 = Random.value;
+                var rand2 = Random.value;
+                var gaussianNumber = Mathf.Sqrt(-2 * Mathf.Log(rand1)) * Mathf.Cos(2 * Mathf.PI * rand2);
+                var mean = (bounds.max + bounds.min) / 2;
+                randomNumber = (gaussianNumber * bounds.standardDeviation) + mean;
+                if (step > 0) randomNumber = Mathf.RoundToInt(randomNumber / step) * step;
+                iterations++;
+            } while ((randomNumber < bounds.min || randomNumber > bounds.max) && iterations < iterationLimit);
+
+            if (iterations == iterationLimit) randomNumber = (bounds.min + bounds.max) / 2.0f;
+            return randomNumber;
+        }
+    }
+}
